cd37d25822721ca0308b3581049ed4b1a1daab57
dk596 <derekk740@gmail.com>
2026-04-14 14:21:13 -0700

    Create initializationfile.h

---
+++ Microcontroller_EE310/Assignments/Assignment 8 IO_Ports_Relay/initializationfile.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   initializationfile.h
+ * Author: derek
+ *
+ * Created on April 14, 2026, 1:39 PM
+ */
+// --------------------------
+// Variables 
+// -------------------------- 
+int zeroth_digit = 0x00;
+int first_digit = 0x00;
+int final_code = 0x00;
+int secret_code = 0x23;
+int escape_check = 0x00;
